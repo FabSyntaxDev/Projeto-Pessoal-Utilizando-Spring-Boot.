cf2f9e6b55b2bf7f2c91b45f1f1ce89c1d17920d
FabSyntaxDev <fabio.dsm.eng.software@gmail.com>
2026-04-27 15:43:16 -0300

    Implementação da funcionalidade básica de cadastro

--- src/main/java/com/example/demo/LoginController.java
+++ src/main/java/com/example/demo/LoginController.java
@@ -1,18 +1,30 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
 
+    // ADICIONE ESTAS DUAS LINHAS ABAIXO:
+    @Autowired
+    private UsuarioRepository repository;
+
     @GetMapping("/login")
     public String login() {
-        return "login"; 
+        return "login";
     }
 
     @GetMapping("/cadastro")
     public String cadastro() {
-        return "cadastro"; 
+        return "cadastro";
+    }
+
+    @PostMapping("/cadastro")
+    public String salvarUsuario(Usuario usuario) {
+        repository.save(usuario);
+        return "redirect:/login";
     }
 }
